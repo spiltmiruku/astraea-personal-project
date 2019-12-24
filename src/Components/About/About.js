42a002ab6444c13bcb6f36d71cd0876a93332580
spiltmiruku <spiltmiruku@gmail.com>
2019-12-23 23:24:41 -0700

    Wrote short description for about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,12 @@ import React from 'react';
 const About = () => {
     return(
         <div>
-            
+            <h1>ASTRAEA</h1>
+            <section className='about'>
+                In Greek mythology, ASTRAEA is the 'star-maiden', and the goddess of innocence. 
+                <br/>
+                ASTRAEA is a lifestyle brand, and the future of travel, however, we are more than a space travel company. We create luxury experiences for our clientele. With choices like the Moon, Jupiter, and Mars, you can choose the ultimate travel destination. 
+            </section>
         </div>
     )
 }
