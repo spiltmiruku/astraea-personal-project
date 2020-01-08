897fdb964dd4b7ca5c20f3a9b6e45f664813be78
spiltmiruku <spiltmiruku@gmail.com>
2020-01-08 15:47:05 -0700

    Repositioned items on trip-confirmation page and about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import './about.css';
-const wings = 'https://astraea-project.s3-us-west-2.amazonaws.com/Wings.png';
+import React from "react";
+import "./about.css";
+const wings = "https://astraea-project.s3-us-west-2.amazonaws.com/Wings.png";
 // import chara from '../../resources/chara.webp';
 
 const About = () => {
-    return(
-        <div className='about-box'>
-        <img src={wings} alt='Wings' className='wings' />
-            <div className='title-content-wrapper'>
-            <h1 className='page-title'>ASTRAEA</h1>
-            <section className='about'>
-                <p>    
-                In Greek mythology, ASTRAEA is the 'star-maiden', and the goddess of innocence. 
-                </p>
-                <br/>
-                <p>
-                ASTRAEA is a lifestyle brand, and the future of travel, however, we are more than a space travel company. We create luxury experiences for our clientele. With choices like the Moon, Mars, and Jupiter, you can choose the ultimate travel destination. 
-                </p>
-            </section>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="about-box">
+      <div className="wing-box">
+        <img src={wings} alt="Wings" className="wings" />
+      </div>
+
+      <div className="title-content-wrapper">
+        <h1 className="page-title">ASTRAEA</h1>
+        <section className="about">
+          <p>
+            In Greek mythology, ASTRAEA is the 'star-maiden', and the goddess of
+            innocence.
+          </p>
+          <br />
+          <p>
+            ASTRAEA is a lifestyle brand, and the future of travel, however, we
+            are more than a space travel company. We create luxury experiences
+            for our clientele. With choices like the Moon, Mars, and Jupiter,
+            you can choose the ultimate travel destination.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/Components/TripConfirmation/TripConfirmation.js
+++ src/Components/TripConfirmation/TripConfirmation.js
@@ -15,7 +15,10 @@ class TripConfirmation extends Component {
         console.log(this.props)
         return (
             <div className='confirmation-welcome-box'>
+                <div className='helmet-box'>
             <img id='helmet' src={helmet} alt='astronaut helmet'/>
+                </div>
+                <div className='welcome-text-box'>
             <h2 className='welcome-msg'>Welcome aboard, {this.props.user && this.props.user.passenger_firstname}
                 </h2>
             <section className='confirmation-welcome'>Welcome to your next adventure! Your trip with ASTRAEA is confirmed.
@@ -26,6 +29,7 @@ class TripConfirmation extends Component {
                 <br/><br/>
                 We look forward to flying with you!
             </section>
+                </div>
             </div>
         )
     }
