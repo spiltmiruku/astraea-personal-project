63c6eb3e1c2dcb4327994b426d550505ce4ea581
spiltmiruku <spiltmiruku@gmail.com>
2020-01-09 18:49:35 -0700

    Re-wrote as join statement

--- db/get_trips.sql
+++ db/get_trips.sql
@@ -1,2 +1,6 @@
-SELECT * FROM astraea_bookings
-WHERE user_id = $1;
+-- SELECT * FROM astraea_bookings
+-- WHERE user_id = $1;
+
+SELECT * FROM astraea_users u 
+JOIN astraea_bookings b ON u.user_id = b.user_id
+WHERE b.user_id = $1;
